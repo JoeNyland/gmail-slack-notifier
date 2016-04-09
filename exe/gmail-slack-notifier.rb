d9992fae69b18eba30f8161ecff9ba260c4ce50b
Joe Nyland <joenyland@me.com>
2016-04-09 15:59:20 +0100

    Initial version of the mail detection loop

--- exe/gmail-slack-notifier.rb
+++ exe/gmail-slack-notifier.rb
@@ -3,3 +3,42 @@
 require 'gmail/slack/notifier'
 
 Gmail::Slack::Notifier.new.run
+
+Gmail.connect!('email','password') do |client|
+
+  # We start monitoring for emails from now onwards
+  last_email_time = Time.now
+
+  # Loop indefinitely, checking for mail every X seconds
+  while true
+
+    # Get all the email in the inbox
+    mail = client.inbox.find
+
+    # Only continue if there's at least one message in the inbox
+    unless mail.empty?
+
+      # Select the messages in the inbox that have been received after the last message that we alerted for
+      mail.select! {|m| Time.parse(m.date).utc > last_email_time.utc }
+
+      # mail now contains new message(s) (if any) that we should alert for
+      unless mail.empty?
+
+        # mail now contains new mail after the last email that we alerted for and there's at least one
+        mail.each do |message|
+          # For now, just puts some basic info about each message
+          puts "New message: #{message.date} -- #{message.subject}"
+        end
+
+        # We want to use the time of the last message on the next iteration of the loop to search for messages after it
+        last_email_time = Time.parse(mail.last.date)
+
+      end
+
+    end
+
+    sleep 10
+
+  end
+
+end
