30b8c34b3a9341c91d4a4e836b03f172e0db085d
Joe Nyland <joenyland@me.com>
2016-04-16 13:07:05 +0100

    Actually get the bot working and sending Slack messages on receiving emails in Gmail!

--- exe/gmail-slack-notifier.rb
+++ exe/gmail-slack-notifier.rb
@@ -19,6 +19,9 @@ OptionParser.new do |opts|
   opts.on('-p', '--gmail-password GMAIL_PASSWORD', 'Gmail password') do |v|
     options[:gmail_password] = v
   end
+  opts.on('-i', '--gmail-poll-interval GMAIL_POLL_INTERVAL', Integer, 'Gmail poll interval') do |v|
+    options[:gmail_poll_interval] = v
+  end
 end.parse!
 
 # Build a notifier object
@@ -34,53 +37,46 @@ notifier = Gmail::Slack::Notifier.new(
 )
 
 # The main mail detection and notification loop
+Gmail.connect!(options[:gmail_email],options[:gmail_password]) do |client|
 
+  # We start monitoring for emails from now onwards
+  last_email_time = Time.now
 
-# Gmail.connect!('email','password') do |client|
-#
-#   # We start monitoring for emails from now onwards
-#   last_email_time = Time.now
-#
-#   # Loop indefinitely, checking for mail every X seconds
-#   while true
-#
-#     # Get all the email in the inbox
-#     mail = client.inbox.find
-#
-#     # Only continue if there's at least one message in the inbox
-#     unless mail.empty?
-#
-#       # Select the messages in the inbox that have been received after the last message that we alerted for
-#       mail.select! {|m| Time.parse(m.date).utc > last_email_time.utc }
-#
-#       # mail now contains new message(s) (if any) that we should alert for
-#       unless mail.empty?
-#
-#         # mail now contains new mail after the last email that we alerted for and there's at least one
-#         mail.each do |message|
-#           # For now, just puts some basic info about each message
-#           puts "New message: #{message.date} -- #{message.subject}"
-#         end
-#
-#         # We want to use the time of the last message on the next iteration of the loop to search for messages after it
-#         last_email_time = Time.parse(mail.last.date)
-#
-#       end
-#
-#     end
-#
-#     sleep 10
-#
-#   end
-#
-# end
-#
-# test_email = {
-#   date:    Time.now,
-#   from:    'foo.bar@example.com',
-#   subject: 'This is a test email from Foo Bar',
-#   excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-# }
-#
-# notifier = Gmail::Slack::Notifier::SlackClient.new "https://hooks.slack.com/services/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"
-# notifier.ping(username: '@joenyland',text: to_slack_message(test_email))
+  # Loop indefinitely, checking for mail every X seconds
+  while true
+
+    # Get all the email in the inbox
+    mail = client.inbox.find
+
+    # Only continue if there's at least one message in the inbox
+    unless mail.empty?
+
+      # Select the messages in the inbox that have been received after the last message that we alerted for
+      mail.select! {|m| Time.parse(m.date).utc > last_email_time.utc }
+
+      # mail now contains new message(s) (if any) that we should alert for
+      unless mail.empty?
+
+        # mail now contains new mail after the last email that we alerted for and there's at least one
+        mail.each do |message|
+          # Send a message to Slack!
+          notifier.slack_client.ping(username: options[:slack_target_channel], text: {
+              date: message.date,
+              from: "#{message.from.first.name} #{message.from.first.mailbox}@#{message.from.first.host}",
+              subject: message.subject
+            })
+        end
+
+        # We want to use the time of the last message on the next iteration of the loop to search for messages after it
+        last_email_time = Time.parse(mail.last.date)
+
+      end
+
+    end
+
+    # Wait for the poll interval before checking for more email, default to 30 seconds
+    sleep options[:gmail_poll_interval] || 30
+
+  end
+
+end
--- lib/gmail/slack/notifier/slack_client.rb
+++ lib/gmail/slack/notifier/slack_client.rb
@@ -1,37 +1,46 @@
 require 'slack-notifier'
 
+# Takes a hash of data and formats it as a string, ready for sending to Slack
+#
+# Example input hash:
+# {
+#   date:    Time.now,
+#   from:    'foo.bar@example.com',
+#   subject: 'This is a test email from Foo Bar',
+#   excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+# }
+#
+# Would result in:
+#   Date: 2016-04-12 10:36:30 +0100
+#   From: foo.bar@example.com
+#   Subject: This is a test email from Foo Bar
+#   Excerpt: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#
+class Hash
+  def format_as_slack_message
+    # ToDo: Refactor me!
+    a = []
+    self.each do |k,v|
+      a << "#{k.capitalize}: #{v}"
+    end
+    a.join "\n"
+  end
+end
+
 module Gmail
   module Slack
     class Notifier
       class SlackClient < ::Slack::Notifier
 
         def initialize(params)
+          # ToDo: Refactor me!
           fail 'A Slack webhook URL is required' if params[:webhook_url].nil?
           super params[:webhook_url], channel: params[:channel].nil? ? nil : params[:channel]
         end
 
-        # Takes a hash of data and formats it as a string, ready for sending to Slack
-        #
-        # Example input hash:
-        # {
-        #   date:    Time.now,
-        #   from:    'foo.bar@example.com',
-        #   subject: 'This is a test email from Foo Bar',
-        #   excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-        # }
-        #
-        # Would result in:
-        #   Date: 2016-04-12 10:36:30 +0100
-        #   From: foo.bar@example.com
-        #   Subject: This is a test email from Foo Bar
-        #   Excerpt: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        #
-        def format_message(hash)
-          a = []
-          hash.each do |k,v|
-            a << "#{k.capitalize}: #{v}"
-          end
-          a.join "\n"
+        def ping(message, options={})
+          message[:text] = message[:text].format_as_slack_message # ToDo: Refactor to be a bang method
+          super message, options
         end
 
       end
